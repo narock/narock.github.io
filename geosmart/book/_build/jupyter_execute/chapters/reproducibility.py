178e9a6d010451340ceb82680b9f922252b0dc6b
Tom Narock <thomas.narock@goucher.edu>
2023-03-04 15:48:34 -0500

    Removed Geosmart Tutorial

--- geosmart/book/_build/jupyter_execute/chapters/reproducibility.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Reproducibility
-# 
-# How to get credit for the work. Steps for getting a DOI. Discuss workflow adaptation to other datasets and science questions if suitable.
-# 
--- geosmart/book/_build/jupyter_execute/chapters/workflow.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Workflow Management 
-# 
-# Explain cloud-based workflows and compute requirements.
-# If you seek to make the model run in production or achieve higher productivity and scalability, use GeoWeaver or other tools.
-# 
